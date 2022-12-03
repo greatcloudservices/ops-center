5d848d07f3660120879199ca2821b575df396f48
Richard Brown <rbrown072@gmail.com>
2022-12-03 00:10:44 -0600

    Added Auth to Amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,35 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "SalesPipeline": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "opscenterdd685506PostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "opscenterdd685506": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string",
+            "GoogleWebClient": "string"
+        },
+        "userPoolGroups": {
+            "EveryoneGroupRole": "string",
+            "AdministratorGroupRole": "string"
+        }
+    }
+}
